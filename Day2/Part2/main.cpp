b1ff6ff2ea9a0c60995fa5b3d81736fcbb197405
lbielenberg <lbielenberg@anybotics.com>
2026-03-26 14:53:11 +0100

    completed day2

---
+++ Day2/Part2/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+#include "StringSanitizer.h"
+
+using namespace StringSanitizer;
+
+int main(){
+    std::vector<std::string> log = {
+        "25.5", "42", "abc", "", "150", "75.0", "-10", "60.5", "Error", "90", "10.0", "100.0", "9.9", "101", "5.4", "0x45", "LEET"
+    };
+
+    
+    auto cleaned_log = filter_and_convert(log);
+
+    std::cout << "Cleaned log entries:" << std::endl;
+    for (const auto& entry : cleaned_log){
+        std::cout << entry << std::endl;
+    }
+    
+    process_data(cleaned_log);
+    std::cout << "\nProcessed data:" << std::endl;
+    for (const auto& entry : cleaned_log){
+        std::cout << entry << std::endl;
+    }
+
+    auto average = average_vector(cleaned_log);
+
+    std::cout << "\nAverage of the signal is: " << average << std::endl;
+    
+    return 0;
+}
